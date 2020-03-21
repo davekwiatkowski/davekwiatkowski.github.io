5d68835c3aa813aefad55bfc1956f125692487b2
dave kwiatkowski <dave@beedrill.lan>
2020-03-20 21:58:44 -0700

    Updates

---
+++ precache-manifest.5fc8674c6da98458b7ddf5cd51c6cd34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce39ecad350616e6f18364b6098adfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7254380b50dcfab8112",
+    "url": "/static/css/main.cb43e5c0.chunk.css"
+  },
+  {
+    "revision": "ddf296f370492210f39b",
+    "url": "/static/js/2.0ab8e73c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0ab8e73c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7254380b50dcfab8112",
+    "url": "/static/js/main.aed3df34.chunk.js"
+  },
+  {
+    "revision": "0b4f3269348c2e5315e5",
+    "url": "/static/js/runtime-main.9089ee33.js"
+  },
+  {
+    "revision": "34db0436869fbe2bbb37aa17eafc5e9e",
+    "url": "/static/media/gif.34db0436.webp"
+  }
+]);
