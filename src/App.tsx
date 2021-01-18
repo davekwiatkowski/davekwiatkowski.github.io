f35b93479ff1abda81fc1734930e74437415651e
Dave Kwiatkowski <github@davekwiatkowski.com>
2021-01-17 22:01:44 -0800

    Make some basic updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import './App.css';
 import React, { FC } from 'react';
 import Link from './components/Link';
-import gif from './images/gif.gif';
 import Links from './constants/Links';
 import Strings from './constants/Strings';
-import Image from './components/Image';
-import IStyleSheet from './shared/IStyleSheet';
 
 const App: FC = () => (
   <div className='App'>
     <div className='Content'>
-      <Image
-        description={'Gif of Joy from Pokémon typing'}
-        source={gif}
-        style={styles.gif}
-      />
-      <p>Hi!</p>
+      <p>Hi! 🌲</p>
       <p>
         I am a software engineer at
         <Link
@@ -56,19 +48,9 @@ const App: FC = () => (
         or messaging me on
         <Link text={Strings.linkedIn} url={Links.linkedIn} isPrecededBySpace />.
       </p>
-      <p>-Dave</p>
+      <p>Dave</p>
     </div>
   </div>
 );
-
-const styles: IStyleSheet = {
-  gif: {
-    width: '100%',
-    maxWidth: '100%',
-    height: 'auto',
-    filter:
-      'drop-shadow(-100px -100px 0px tan) brightness(1.5) hue-rotate(-50deg)',
-  },
-};
 
 export default App;
