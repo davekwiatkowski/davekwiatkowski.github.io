f3ba1d7357c4323166392dd409765376bb5a8e53
Dave Kwiatkowski <github@davekwiatkowski.com>
2022-01-03 09:16:04 -0500

    Add spell check

--- blog/schemas/blockContent.js
+++ blog/schemas/blockContent.js
@@ -16,26 +16,29 @@ export default {
     {
       title: 'Block',
       type: 'block',
+      options: {
+        spellCheck: true,
+      },
       // Styles let you set what your user can mark up blocks with. These
       // correspond with HTML tags, but you can set any title or value
       // you want and decide how you want to deal with it where you want to
       // use your content.
       styles: [
-        {title: 'Normal', value: 'normal'},
-        {title: 'H1', value: 'h1'},
-        {title: 'H2', value: 'h2'},
-        {title: 'H3', value: 'h3'},
-        {title: 'H4', value: 'h4'},
-        {title: 'Quote', value: 'blockquote'},
+        { title: 'Normal', value: 'normal' },
+        { title: 'H1', value: 'h1' },
+        { title: 'H2', value: 'h2' },
+        { title: 'H3', value: 'h3' },
+        { title: 'H4', value: 'h4' },
+        { title: 'Quote', value: 'blockquote' },
       ],
-      lists: [{title: 'Bullet', value: 'bullet'}],
+      lists: [{ title: 'Bullet', value: 'bullet' }],
       // Marks let you mark up inline text in the block editor.
       marks: {
         // Decorators usually describe a single property – e.g. a typographic
         // preference or highlighting by editors.
         decorators: [
-          {title: 'Strong', value: 'strong'},
-          {title: 'Emphasis', value: 'em'},
+          { title: 'Strong', value: 'strong' },
+          { title: 'Emphasis', value: 'em' },
         ],
         // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
@@ -59,7 +62,7 @@ export default {
     // as a block type.
     {
       type: 'image',
-      options: {hotspot: true},
+      options: { hotspot: true },
     },
   ],
-}
+};
