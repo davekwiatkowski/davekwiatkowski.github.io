2a5911ad72c1bbd36f47f57f1ca532be1c61ddc0
dave-kwiatkowski <dave.kwiatkowski@uipath.com>
2023-01-16 23:14:00 -0800

    update home page text

--- website/src/pages/HomePage.tsx
+++ website/src/pages/HomePage.tsx
@@ -9,20 +9,13 @@ import Tab from '../components/common/tabs/Tab';
 const HomePage: FC<{ tab: 'blog' | 'runs' }> = ({ tab }) => (
   <div className="flex justify-center w-full pt-4 pb-4 md:pt-12 md:pb-12">
     <div className="w-full max-w-7xl">
-      <div className="flex flex-row flex-wrap h-auto gap-4 pt-4 pb-4 pl-4 pr-4 md:pt-0 md:pb-0 sm:flex-nowrap">
+      <div className="flex items-end flex-row flex-wrap h-auto gap-4 pt-4 pb-4 pl-4 pr-4 md:pt-0 md:pb-0 sm:flex-nowrap">
         <h1 className="w-full font-thin text-3xl text-left sm:text-4xl md:text-5xl lg:text-7xl sm:w-1/2 lg:w-2/3 text-PRIMARY">
           Dave Kwiatkowski
         </h1>
         <div className="w-full text-sm font-light sm:w-1/2 lg:w-1/3">
           <ul className='list-["-_"] list-inside text-TEXT_DE_EMP'>
-            <li>currently just a runner and a coding person</li>
-            <li>
-              supported by
-              {' '}
-              <Button href="https://www.instagram.com/seattlerunning/">
-                @seattlerunning
-              </Button>
-            </li>
+            <li>a guy who runs a lot and writes some code</li>
             <li>
               my
               {' '}
