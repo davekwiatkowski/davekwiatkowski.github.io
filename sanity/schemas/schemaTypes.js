600a8e16995eafa2bed475e887239c74b8f746a1
Dave <github@davekwiatkowski.com>
2025-06-17 16:01:03 -0400

    Remove post

--- sanity/schemas/schemaTypes.js
+++ sanity/schemas/schemaTypes.js
@@ -1,6 +1,5 @@
 // We import object and document schemas
 import blockContent from './blockContent';
-import post from './post';
 import run from './run';
 import distance from './distance';
 import duration from './duration';
@@ -12,9 +11,6 @@ import mainLink from './mainLink';
 
 // Then we give our schema to the builder and provide the result to Sanity
 const schemaTypes = [
-  // The following are document types which will appear
-  // in the studio.
-  post,
   // When added to this list, object types can be used as
   // { type: 'typename' } in other document schemas
   blockContent,
